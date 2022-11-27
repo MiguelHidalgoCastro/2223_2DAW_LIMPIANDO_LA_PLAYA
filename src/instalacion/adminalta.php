a2441173f222c1823f66fefa2476eca849e89ce8
Antonio Moruno <antmoruno4@gmail.com>
2022-11-27 21:05:28 +0100

    Adición de documentación (análisis, diseño, sql). Corrección de errores para funcionamiento en servidor

--- src/instalacion/adminalta.php
+++ src/instalacion/adminalta.php
@@ -2,13 +2,13 @@
 	require_once('conexion.php');
 	include('../php/modelos/modelo.php');
  
- if ( isset($_POST['usuario']) && !empty($_POST['usuario']) &&
- isset($_POST['password']) && !empty($_POST['password'])){
+	if ( isset($_POST['usuario']) && !empty($_POST['usuario']) &&
+	isset($_POST['password']) && !empty($_POST['password'])){
 		
-	$nombre = $_POST['usuario'];
-	$pass = $_POST['password'];
-	$perfil= 'LP';
-	$hass = password_hash($pass, PASSWORD_DEFAULT);
+		$nombre = $_POST['usuario'];
+		$pass = $_POST['password'];
+		$perfil= 'LP';
+		$hass = password_hash($pass, PASSWORD_DEFAULT);
 		
 		$consulta = $mysqli->prepare ("INSERT INTO Administrador (nombre,clave,perfil) VALUES (?,?,?)");
 		$consulta->bind_param('sss',$nombre,$hass,$perfil);
@@ -21,6 +21,9 @@
 		$fichero= '../instalacion';
 		$objeto = new Modelo();
 		$objeto->borrarFichero($fichero);
-	 }
-        header("Location:../php/vistas/inicio_sesion.html");
+		header("Location:../php/vistas/inicio_sesion.html");
+	}
+	else{
+		header("Location:alta_usuario.html");
+	}
 ?>   
--- src/php/config/conexion.php
+++ src/php/config/conexion.php
@@ -6,16 +6,16 @@ define("PASSWORD","Q{Hq-DjOPX02");
 define("BBDD",'user2daw_BD1-15');*/
 
 /*Conexión Antonio*/
-/*DEFINE('SERVIDOR', '2daw.esvirgua.com');
+DEFINE('SERVIDOR', '2daw.esvirgua.com');
 DEFINE('USUARIO', 'user2daw_09');
 DEFINE('PASSWORD', 'v;$^Z]4xXphS');
-DEFINE('BD', 'user2daw_BD1-09');*/
+DEFINE('BBDD', 'user2daw_BD1-09');
 
 
-define("SERVIDOR",'localhost');
+/*define("SERVIDOR",'localhost');
 define("USUARIO",'root');
 define("PASSWORD","");
-define("BBDD",'login');
+define("BBDD",'login');*/
 //$mysqli=new mysqli(SERVIDOR,USUARIO,PASSWORD,BBDD);
 
 ?>
