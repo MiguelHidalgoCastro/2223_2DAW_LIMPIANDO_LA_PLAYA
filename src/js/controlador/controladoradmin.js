952db35085bb07f172edf14e7995f243e8415f88
Antonio Moruno <antmoruno4@gmail.com>
2022-12-14 22:07:17 +0100

    nombre archivo mayúscula arreglado

---
+++ src/js/controlador/controladoradmin.js
@@ -0,0 +1,37 @@
+/**
+    @file Contiene el controlador principal de la aplicación
+    @author Free Chapapote <albertosalazarmunoz.guadalupe@alumnado.fundacionloyola.net>
+    @license GPL-3.0-or-later
+**/
+
+import { VistaAdmin } from '../vistas/vistaadmin.js'
+
+
+
+/**
+ * Controlador de la aplicación
+ */
+class Controlador {
+
+    /**
+     * Constructor de la aplicación
+     * Al cargar la página llama a la funcion iniciar
+     */
+    constructor() {
+        window.onload = this.iniciar.bind(this)
+    }
+
+    /**
+     * Solo crea las vistas por ahora
+     */
+    iniciar() {
+
+        this.nav = document.getElementsByTagName('nav')[0] //Buscamos el elemento nav
+
+        this.formularioLogin = document.getElementsByTagName('main')[0]
+        this.vistaAdmin = new VistaAdmin(this.formularioLogin)
+
+    }
+}
+
+new Controlador()
